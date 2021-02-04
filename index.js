d13c52a9d458a99ad47844db5e1fe1ed863db98a
zhangsan <490379191@qq.com>
2021-02-04 14:27:02 +0800

    添加js内容

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2021-02-04 14:23:59
+ * @LastEditors: just
+ * @LastEditTime: 2021-02-04 14:24:44
+ */
+const btn = document.getElementById("btn");
+
+btn.onclick = _ => {
+    alert("您好git")
+}
